333525c3a757c7e933e87ebb700ca9c893111dfd
goodtaeeun <goodtaeeun@kaist.ac.kr>
2024-04-08 23:32:52 +0000

    update to more useful test

--- tests/stdin/stdin.c
+++ tests/stdin/stdin.c
@@ -5,9 +5,24 @@
 #include <stdio.h>
 
 int main () {
-  int ch;
-  while ((ch=getchar()) != EOF) {
-    printf("%c\n", ch);
+  int ch[12];
+
+  for (int i = 0; i < 12; i++) {
+    ch[i] = getchar();
   }
+
+  for (int i = 0; i < 12; i+=3) {
+    int c=0; c += (ch[i] + 3);
+    printf("%d", c);
+  }
+
+  int x;
+  x = 3;
+  if (ch[0] == 'a') {
+    x = ch[1] + ch[2];
+    printf("%d\n", x);
+  }
+
+  printf("%d\n", x);
 
 }
